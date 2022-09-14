f7a20085275793888c35793e81fa422a1b0e03ab
emreeren <emre.eren@emlakjet.com>
2022-09-14 18:36:38 +0300

    update advert status use case handler test added.

---
+++ panel-domain/src/test/java/com/emlakjet/publishing/usecase/UpdateAdvertStatusUseCaseHandlerTest.java
@@ -0,0 +1,33 @@
+package com.emlakjet.publishing.usecase;
+
+import com.emlakjet.advert.port.AdvertPort;
+import com.emlakjet.advert.port.FakeAdvertPort;
+import com.emlakjet.publishing.enums.AdvertStatus;
+import com.emlakjet.publishing.port.FakeAdvertPublishingPort;
+import com.emlakjet.publishing.port.AdvertPublishingPort;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UpdateAdvertStatusUseCaseHandlerTest {
+
+    private final AdvertPort advertPort = new FakeAdvertPort();
+    private final AdvertPublishingPort publishingPort = new FakeAdvertPublishingPort();
+    private final UpdateAdvertStatusUseCaseHandler useCaseHandler = new UpdateAdvertStatusUseCaseHandler(advertPort, publishingPort);
+
+    @Test
+    void should_handle_update_advert_status() {
+
+        // Given
+        var updateStatusUseCase = new UpdateAdvertStatusUseCase(1L, AdvertStatus.NOT_PUBLISHED);
+
+        // When
+        var result = useCaseHandler.handle(updateStatusUseCase);
+
+        // Then
+        assertEquals(1L, result.advertId());
+        assertEquals(AdvertStatus.NOT_PUBLISHED, result.advertStatus());
+
+    }
+
+}
